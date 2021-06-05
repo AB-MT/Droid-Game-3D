7ad111bf38978d2460a0dd72c625a395f3052f84
Mark Kim <m7db@mail.ru>
2021-06-05 10:17:16 +0000

    Upload New File

---
+++ src/audio.py
@@ -0,0 +1,42 @@
+from direct.showbase.DirectObject import DirectObject
+from direct.showbase.Audio3DManager import Audio3DManager
+
+manager = None
+enabled = True
+
+# Специальный класс звуков, которые не теряются на фоне других
+
+class FlatSound(DirectObject):
+    def __init__(self, file, volume=1.0):
+        if enabled:
+            self.sound = loader.loadSfx(file)
+        self.filename = file
+        self.setVolume(volume)
+
+    def setVolume(self, volume):
+        if enabled:
+            self.sound.setVolume(volume)
+
+    def getVolume(self):
+        if enabled:
+            return self.sound.getVolume()
+        else:
+            return 0.0
+
+    def isPlaying(self):
+        if enabled:
+            return self.sound.status() == 2
+        else:
+            return False
+
+    def play(self):
+        if enabled:
+            self.sound.play()
+
+    def setLoop(self, loop):
+        if enabled:
+            self.sound.setLoop(loop)
+
+    def stop(self):
+        if enabled:
+            self.sound.stop()
