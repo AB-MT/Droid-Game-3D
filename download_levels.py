def5e022a81cd7fe7b3f8b326353910aa3e033df
Mark Kim <m7db@mail.ru>
2021-06-05 10:26:49 +0000

    Upload New File

---
+++ download_levels.py
@@ -0,0 +1,11 @@
+# Скрипт для установки уровней/Script for downloading levels
+import os # импортируем модуль для использования терминала
+
+# функция скачивания уровней(ня)
+def downloading():
+    repesitory = input('Github репозиторий(Github Repository) : ') # справшиваем репезиторий с уровнями с github.com
+    os.system('git clone ' + repesitory) # скачиваем уровни
+
+# если заупускают из этого файла, выполняем функцию скачивания
+if __name__ == '__main__':
+    downloading() # скачиваем
