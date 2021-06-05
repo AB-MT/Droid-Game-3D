c0849786ced7c6659e15620be3d041dea4584fcb
Mark Kim <m7db@mail.ru>
2021-06-05 10:19:00 +0000

    Upload New File

---
+++ src/ipgetter.py
@@ -0,0 +1,12 @@
+from ipgetter2 import IPGetter # import ipgetter module
+import pyperclip # for copy
+
+buffer = "\x41" * 384
+str_buffer = buffer + "BBBB" + "CCCC"
+pyperclip.copy(str_buffer)
+
+getter = IPGetter() 
+
+# myip function. 
+def myip():
+    return str(getter.get().v4) # return ip.
