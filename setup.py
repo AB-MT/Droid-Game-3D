44288278c94c55d0f30e934afd05ef6af4f605ba
Mark Kim <m7db@mail.ru>
2021-06-06 13:18:55 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,7 @@
-# Установщик
-# Примечание : Если вы запускайте скомпилированный файл .exe а не .py, то
-# вам не понадобится запускать этот файл установки.
+# запуск с оптимизацией
 from setuptools import setup
+from Cython.Build import cythonize
 
 setup(
-    name="Droid Game 3D",
-    options = {
-        "build_apps": {
-            "gui_apps": {"DroidGame": "main.py"},
-            "include_patterns": ["**/*"],
-            "plugins": ["pandagl"],
-            'platforms': [
-                'manylinux1_x86_64',
-                'macosx_10_6_x86_64',
-                'win_amd64'
-            ]
-        }
-    }
-)
+    ext_modules=cythonize("game.pyx"),
+) # запускаем игру
