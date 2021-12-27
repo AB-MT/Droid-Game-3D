e404d7201d4055a1f2766c17ec036bc353177f92
WennMarcoRTX <96744017+WennMarcoRTX@users.noreply.github.com>
2021-12-27 22:04:37 +0300

    edited comments for setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-# запуск с оптимизацией
-from setuptools import setup
-from Cython.Build import cythonize
+# компиляция в C код
+from setuptools import setup # модуль для сетапа
+from Cython.Build import cythonize # ситон(пайтон с расшерениями С)
 
 setup(
     ext_modules=cythonize("game.pyx"),
-) # запускаем игру
+) # компилим
