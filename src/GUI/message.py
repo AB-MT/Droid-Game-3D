06aa873b69358be93d6a2b5d0a6a2c244e789ae6
Left Chel <96744017+WennMarcoRTX@users.noreply.github.com>
2022-02-11 14:09:01 +0300

    Add files via upload

---
+++ src/GUI/message.py
@@ -0,0 +1,18 @@
+import PySimpleGUI as sg
+
+def message(message):
+    sg.theme('DarkAmber')   
+    
+    layout = [  [sg.Text(message)],
+                [sg.Button('Ok')]
+            ]
+
+    
+    window = sg.Window('Message', layout)
+    
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == 'Ok': 
+            break
+
+    window.close()
