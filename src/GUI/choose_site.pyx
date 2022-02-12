8c0f17cf3d7b6994bdfe783a5bd655126f9522b1
Left Chel <96744017+WennMarcoRTX@users.noreply.github.com>
2022-02-12 20:25:33 +0300

    Create choose_site.pyx

---
+++ src/GUI/choose_site.pyx
@@ -0,0 +1,11 @@
+import pyximport # импоритурем Cython
+pyximport.install() # инициализируем его
+import PySimpleGUI as sg
+
+def site(site):
+    sg.popup('You connect to game site : ' + site)
+
+    return site
+
+if __name__ == '__main__':
+    site('github.com')
