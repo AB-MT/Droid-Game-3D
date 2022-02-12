7742c47781658b8f3af694aa6e34cfb3875eabfd
Left Chel <96744017+WennMarcoRTX@users.noreply.github.com>
2022-02-12 11:37:58 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,36 @@
-# компиляция в C код
+# setup of game/установка игры
+from src.settings import * # импортируем настройки
+from colorama import init, Fore, Back, Style # импортируем цветовую систему
+import sys # система
 from setuptools import setup # модуль для сетапа
 from Cython.Build import cythonize # ситон(пайтон с расшерениями С)
 
-setup(
-    ext_modules=cythonize("game.pyx"),
-) # компилим
+init(autoreset=True)
+
+# приветствие
+def welcome():
+    print(Fore.BLACK + Back.GREEN + Style.DIM + f'Welcome to {NAME} version {VERSION}') # печатаем черный текст на зеленом фоне
+
+
+def go_or_no():
+    print(f'Now you can install {NAME}') # поехали
+    go = input('Go?[Y/n]') # поехали?
+    if go == None or go == '':
+        setup(
+            ext_modules=cythonize("game.pyx"),
+        ) # компилим
+
+    elif go.lower() == 'y':
+        setup(
+            ext_modules=cythonize("game.pyx"),
+        ) # компилим
+
+
+    else:
+        print('Ok, bye!')
+        print(f'Exiting from {NAME}...')
+        sys.exit()
+
+
+welcome()
+go_or_no()
